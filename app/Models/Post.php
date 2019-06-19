afb78ef18f4f0ed6799498040fdabf9c49550af9
ky0he1 <kyouta1218@gmail.com>
2019-06-19 16:18:25 +0900

    Fix relation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,6 +12,11 @@ class Post extends Model
 
     public function user()
     {
-        return $this->belongsTo('App\Models\User');
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
     }
 }

13b1d56534091c10fc5cb48b66a6fdb42b49b8ae
ky0he1 <kyouta1218@gmail.com>
2019-06-19 16:13:32 +0900

    Add send user input

--- resources/js/pages/PostsDetail.vue
+++ resources/js/pages/PostsDetail.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="container mt-4">
+  <div>
 
     <h1>詳細</h1>
 
-    <div class="card">
+    <div class="card mb-4">
+      <h5 class="card-header">投稿</h5>
       <div class="card-body">
         <div class="card-text">
           <p><i class="fas fa-user mr-2"></i>サンプルユーザー</p>
@@ -11,9 +12,22 @@
             <p class="text-right">{{ post.created_at }}</p>
             <div class="row justify-content-end">
               <router-link to="/" class="mr-2"><button class="btn btn-outline-secondary">戻る</button></router-link>
-              <Modal @setUserInput="setUserInput" :modalTitle="modalTitle" class="mr-3" />
+              <Modal @sendUserInput="sendUserInput" :modalTitle="modalTitle" class="mr-3" />
             </div>
         </div>
+      </div>
+    </div>
+
+    <div class="card">
+      <h5 class="card-header">コメント一覧</h5>
+      <div class="card-body">
+        <ul class="list-group list-group-flush">
+          <li v-for="(comment, index) in post.comments" :key="index" class="list-group-item">
+            <p><i class="fas fa-user mr-2"></i>サンプルユーザー</p>
+            <p class="ml-2">{{ comment.comment }}</p>
+            <p class="text-right">{{ comment.created_at }}</p>
+          </li>
+        </ul>
       </div>
     </div>
 
@@ -31,25 +45,43 @@ export default {
   data () {
     return {
       modalTitle: 'コメント',
-      post: []
+      post: {},
+      userInput: {
+        'comment': '',
+        'post_id': '',
+        'user_id': 1,     // test user
+      }
     }
   },
 
 
   created () {
-    axios.get(`/api/posts/${this.$route.params.id}`)
-      .then(response => {
-        this.post = response.data.post
-      })
-      .catch(error => {
-        console.log(error)
-      })
+    this.getData()
   },
 
   methods: {
-    setUserInput (payload) {
-      this.userInput = payload
-    }
+    getData () {
+      axios.get(`/api/posts/${this.$route.params.id}`)
+        .then(response => {
+          this.post = response.data.post
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+
+    sendUserInput (payload) {
+      this.userInput.comment = payload
+      this.userInput.post_id = Number(this.$route.params.id)
+
+      axios.post('/api/comments', this.userInput)
+        .then(response => {
+          this.getData()
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
   }
 }
 </script>

d5d8e7210785fb87c96510a9ce44e669475670a8
ky0he1 <kyouta1218@gmail.com>
2019-11-08 11:34:58 +0900

    Supports jwt auth for path

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,25 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['middleware' => ['api']], function(){
-    Route::apiResource('/posts', 'Api\PostController');
-    Route::apiResource('/comments', 'Api\CommentController');
+    Route::apiResource('/posts', 'Api\PostController')
+        ->only(['index', 'show']);
+    Route::apiResource('/comments', 'Api\CommentController')
+        ->only(['index', 'show']);
+
+    Route::post('register', 'JWTAuthController@register')
+        ->name('api.jwt.register');
+    Route::post('login', 'JWTAuthController@login')
+        ->name('api.jwt.login');
+});
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::apiResource('/posts', 'Api\PostController')
+        ->only(['create']);
+    Route::apiResource('/comments', 'Api\CommentController')
+        ->only(['create']);
+
+    Route::get('user', 'JWTAuthController@user')
+        ->name('api.jwt.user');
+    Route::post('logout', 'JWTAuthController@logout')
+        ->name('api.jwt.logout');
 });
